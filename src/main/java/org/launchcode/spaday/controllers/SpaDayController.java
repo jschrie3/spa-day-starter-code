9b52b248e81239e5d5356eb6ebb676333601ec73
julian <julianschriedel@gmail.com>
2023-03-21 15:26:06 -0500

    finished main studio, starting bonus missions

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -5,11 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
+// CHAPTER 13 STUDIO: SPA DAY
 
 @Controller
 public class SpaDayController {
 
+    // The owners gave us this method to help us figure out which facial treatments are appropriate for which skin type.
     public boolean checkSkinType(String skinType, String facialType) {
+        // basic structure:
+        // takes in skintype and facial type
+        // if skinType is THIS, then THESE are the facialType options
+
         if (skinType.equals("oily")) {
             return facialType.equals("Microdermabrasion") || facialType.equals("Rejuvenating");
         }
@@ -47,21 +53,44 @@ public class SpaDayController {
         return html;
     }
 
+    // The spaMenu() method, which we will use to bring in our Thymeleaf template, menu.html.
     @PostMapping(value="")
     public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String manipedi, Model model) {
 
+        // Also, our teammates already set up code in our spaMenu() method to fill an ArrayList<String>
+        // with facial treatments that would work for the user’s skin type.
+        //Now, we just need to use Thymeleaf to display the appropriate facial treatments
+        // (stored in the ArrayList, appropriateFacials)!
+        // Let’s head back to menu.html and checkout the empty div with the id, servicesMenu.
+
+        // adding facial menu options to array list called facials
         ArrayList<String> facials = new ArrayList<>();
         facials.add("Microdermabrasion");
         facials.add("Hydrofacial");
         facials.add("Rejuvenating");
         facials.add("Enzyme Peel");
 
+        // adding only appropriate facials for skinType options to each facial menu item
         ArrayList<String> appropriateFacials = new ArrayList<>();
+
+        // for each facial menu item in facials array list
         for (int i = 0; i < facials.size(); i ++) {
+
+            // call check checkSkinType to check user input for skinType
+            // then add the returned values (appropriate facials for that skinType)
+            // to the appropriateFacials arraylist
+
             if (checkSkinType(skintype,facials.get(i))) {
                 appropriateFacials.add(facials.get(i));
             }
         }
+
+        // need to create models to bring data into the view
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
+        model.addAttribute("facials", facials);
+        model.addAttribute("appropriateFacials", appropriateFacials);
 
         return "menu";
     }
